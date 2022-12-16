6a51d64ce3cf1ba18956599e439ceb80aa9bcc20
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2022-12-16 20:12:04 +0100

    add basic gui setup

--- gui/app.py
+++ gui/app.py
@@ -3,3 +3,12 @@ import customtkinter
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+
+        self.setup_app()
+
+    def setup_app(self):
+        customtkinter.set_appearance_mode('dark')
+        customtkinter.set_default_color_theme('green')
+
+        self.title('is_nic_cage')
+        self.geometry('1200x600')
