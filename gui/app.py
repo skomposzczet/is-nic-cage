31de8497b25d5737c175c5fe1f22f68ea51dd634
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2022-12-16 20:50:51 +0100

    add buttons

--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,5 @@
 import customtkinter
+from tkinter import ttk
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -8,6 +9,7 @@ class App(customtkinter.CTk):
 
         self.setup_sidebar()
         self.setup_main_frame()
+        self.setup_buttons()
 
     def setup_app(self):
         customtkinter.set_appearance_mode('dark')
@@ -16,6 +18,7 @@ class App(customtkinter.CTk):
         self.title('is_nic_cage')
         self.geometry('1200x600')
         self.minsize(900, 400)
+        self.font = ('TkFixedFont', 14)
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -27,3 +30,38 @@ class App(customtkinter.CTk):
     def setup_main_frame(self):
         self.main_frame = customtkinter.CTkFrame(self)
         self.main_frame.grid(row=0, column=1, padx=20, pady=20, sticky='news')
+
+    def setup_buttons(self):
+        self.create_files_button()
+        self.create_web_button()
+        self.create_separator()
+        self.create_apply_button()
+
+    def create_files_button(self):
+        self.load_button_files = customtkinter.CTkButton(self.sidebar, text='Load image from files', font=self.font, command=self.callback_files)
+        self.load_button_files.grid(row=0, padx=20, pady=20, sticky='ew')
+
+    def create_web_button(self):
+        self.load_button_web = customtkinter.CTkButton(self.sidebar, text='Load image from web', font=self.font, command=self.callback_web)
+        self.load_button_web.grid(row=1, padx=20, pady=10, sticky='ew')
+
+    def create_separator(self):
+        ttk.Separator(self.sidebar, orient='vertical').grid(row=2, sticky='ew', padx=10, pady=10)
+
+    def create_apply_button(self):
+        self.apply_button = customtkinter.CTkButton(self.sidebar, text='CHECK IF CAGE', font=self.font, command=self.callback_apply, state='disabled', fg_color='SpringGreen4')
+        self.apply_button.grid(row=3, padx=20, pady=10, sticky='ew')
+
+    def callback_files(self):
+        self.unlock_apply_button()
+        print('files')
+
+    def callback_web(self):
+        self.unlock_apply_button()
+        print('web')
+    
+    def unlock_apply_button(self):
+        self.apply_button.configure(state='normal')
+
+    def callback_apply(self):
+        print('apply')
